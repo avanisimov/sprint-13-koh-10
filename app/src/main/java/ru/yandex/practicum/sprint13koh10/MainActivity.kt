f9acc3fe98844bfbebd5130bc982aad5e254c28a
Aleksei Anisimov <suxelshock@gmail.com>
2023-12-11 19:29:32 +0300

    1 Исправлено отображение заголовка тулбара в зависимости от текущего выбранного таба в боттомнавигейшон. Правки после ПР: строковый ресурс для заголовка добавлен в ScreenMode

--- app/src/main/java/ru/yandex/practicum/sprint13koh10/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint13koh10/MainActivity.kt
@@ -181,15 +181,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun changeCurrentScreenMode(newScreenMode: ScreenMode) {
         if (newScreenMode != currentScreenMode) {
+            binding.toolbar.setTitle(newScreenMode.titleResId)
             when (newScreenMode) {
                 ScreenMode.CATALOG -> {
-                    binding.toolbar.setTitle(R.string.catalog_title)
                     binding.catalogContainer.visibility = View.VISIBLE
                     binding.cartContainer.visibility = View.GONE
                 }
 
                 ScreenMode.CART -> {
-                    binding.toolbar.setTitle(R.string.cart_title)
                     binding.catalogContainer.visibility = View.GONE
                     binding.cartContainer.visibility = View.VISIBLE
                 }
